6c0d0f55b6a7072cafe1a6ebe40cda03e43afedd
Alvaro <alvarojimenez95@hotmail.com>
2016-01-14 12:37:23 +0100

    version 3

--- src/arrays/Coleccion.java
+++ src/arrays/Coleccion.java
@@ -30,15 +30,38 @@ public class Coleccion {
 		}
 	}
 	
-	public static void minimoColeccion(){
-		
+	public double minimoColeccion(){
+		double minimo = vector[0][0];
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 0; j < vector.length; j++) {
+				if (vector[i][j] < minimo)
+					minimo = vector[i][j];
+			}
+		}
+		return minimo;
 	}
 	
-	public static void maximoColeccion(){
-		
+	public double maximoColeccion(){
+		double maximo = vector[0][0];
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 0; j < vector.length; j++) {
+				if (vector[i][j] > maximo)
+					maximo = vector[i][j];
+			}
+		}
+		return maximo;
 	}
 	
-	public static void mediaColeccion(){
-		
+	
+	public double mediaColeccion(){
+		double media = 0;
+		double suma = 0;
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 0; j < vector[i].length; j++) {
+				suma += vector[i][j];
+			}
+		}
+		media = suma / (vector.length * vector[0].length);
+		return media;
 	}
 }
--- src/arrays/TestColeccion.java
+++ src/arrays/TestColeccion.java
@@ -3,8 +3,13 @@ package arrays;
 public class TestColeccion {
 
 	public static void main(String[] args) {
-		Coleccion c = new Coleccion(4,4);
-		c.rellenarColeccion(10);
-		c.mostrarColeccion();
+		Coleccion c1 = new Coleccion(4,4);
+		c1.rellenarColeccion(10);
+		c1.mostrarColeccion();
+		System.out.printf("Valor minimo: %.2f", c1.minimoColeccion());
+		System.out.printf("Valor maximo: %.2f",c1.maximoColeccion());
+		System.out.printf("Valor medio: %.2f",c1.mediaColeccion());
+		
+		
 	}
 }
